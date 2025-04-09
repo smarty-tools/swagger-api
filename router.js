392d026d558c0da27cb86996116e8b2bec8ffbb7
hejuxin <cuobingfeicuo@126.com>
2025-04-09 22:24:31 +0800

    feat: 抽离生成api文件逻辑

--- router.js
+++ router.js
@@ -8,8 +8,7 @@ const Router = require("@koa/router");
 const page = new Router();
 
 
-const { getTemplateHeader, getApi } = require("./utils");
-const getArgs = require("./utils/utils");
+const { getRequestFile } = require("./utils/request");
 
 page.get("/", async (ctx) => {
   const filePath = path.resolve(__dirname, "index.html");
@@ -18,8 +17,6 @@ page.get("/", async (ctx) => {
   ctx.body = html;
 });
 
-const parsed = getArgs(process.argv.slice(2));
-
 page.post("/upload", upload.single('file'), async (ctx) => {
   try {
     const body = ctx.request.body;
@@ -27,29 +24,8 @@ page.post("/upload", upload.single('file'), async (ctx) => {
     options.prefix = !!body.prefix;
 
     const json = JSON.parse(ctx.file.buffer.toString());
+    const str = await getRequestFile(json, options);
 
-    let str = "";
-    const baseUrl = json.servers[0].url;
-    str += getTemplateHeader({ baseUrl });
-
-    const paths = Object.keys(json.paths);
-
-    paths.forEach((path) => {
-      const info = json.paths[path];
-      const method = info["get"] ? "get" : "post";
-
-      const template = getApi({ path, method, ...info[method] }, options);
-
-      str += `${template} \n`;
-    });
-
-    let outPath = path.resolve(__dirname, "api.js");
-    const _path = parsed.o;
-    if (_path && typeof _path !== "boolean") {
-      outPath = path.resolve(_path, "api.js");
-    }
-
-    await fs.writeFile(outPath, str);
     ctx.body = str;
   } catch (e) {
     console.error(e);
---
+++ utils/request.js
@@ -0,0 +1,61 @@
+const fs = require("fs/promises");
+const path = require("path");
+const getArgs = require("./utils");
+
+const parsed = getArgs(process.argv.slice(2));
+
+function getTemplateHeader(info) {
+  return `
+const baseUrl = "${info.baseUrl}";
+`
+};
+
+
+function getApi(params, options) {
+  const payload = params.method === "get" ? "{ params }" : "params";
+  const url = options.prefix ? `\`\${baseUrl}${params.path}\`` : `"${params.path}"`;
+
+  return `
+/**
+ * ${params.summary || "请补充描述..."}
+ * @param {*} params
+ * @returns
+ */
+export const ${params.operationId} = async (params) => {
+  const response = await axiosInstance.${params.method}(${url}, ${payload});
+
+  return response;
+};
+`
+};
+
+async function getRequestFile(json, options) {
+  let str = "";
+  const baseUrl = json.servers[0].url;
+  str += getTemplateHeader({ baseUrl });
+
+  const paths = Object.keys(json.paths);
+
+  paths.forEach((path) => {
+    const info = json.paths[path];
+    const method = info["get"] ? "get" : "post";
+
+    const template = getApi({ path, method, ...info[method] }, options);
+
+    str += `${template} \n`;
+  });
+
+  let outPath = path.resolve(__dirname, "api.js");
+  const _path = parsed.o;
+  if (_path && typeof _path !== "boolean") {
+    outPath = path.resolve(_path, "api.js");
+  }
+
+  await fs.writeFile(outPath, str);
+
+  return str;
+};
+
+module.exports = {
+  getRequestFile
+}
